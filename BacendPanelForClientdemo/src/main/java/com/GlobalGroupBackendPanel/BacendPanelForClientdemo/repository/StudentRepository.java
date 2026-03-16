2c7c56bef79e7e7a12ecd134687b7dfe0031deb7
Jelalletdin91 <jelalletdinberjanow797@gmail.com>
2026-03-16 20:12:57 +0300

    Delete BacendPanelForClientdemo directory

--- BacendPanelForClientdemo/src/main/java/com/GlobalGroupBackendPanel/BacendPanelForClientdemo/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GlobalGroupBackendPanel.BacendPanelForClientdemo.repository;
-
-import com.GlobalGroupBackendPanel.BacendPanelForClientdemo.entity.StudentKimlik;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentKimlik, Long> {
-
-
-}
